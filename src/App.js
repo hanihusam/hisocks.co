16370b6c306d6a285944d19a9e7f79be330c6e3e
Hani Husamuddin <hani.husam@gmail.com>
2020-12-18 11:00:06 +0700

    create the avatar section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import styled from 'styled-components'
-import BgImg from './images/background.jpg'
+import React from "react";
+import styled from "styled-components";
+import BgImg from "./images/background.jpg";
+import Logo from "./images/hisocks-logo-circle.png";
 
 const App = () => {
   return (
@@ -8,38 +9,53 @@ const App = () => {
       <AppMain>
         <MainWrapper>
           <MainCover>
-
+            <ContentWrapper>
+              <Content>
+                <AvatarWrapper>
+                  <ProfilePicture>
+                    <Avatar>
+                      <div className="responsive-sizer" />
+                      <div className="ava" />
+                    </Avatar>
+                  </ProfilePicture>
+                  <TitleWrapper>
+                    <h3 className="display-3">Hisocks</h3>
+                    <p className="subtitle">Kaos Kaki Murah Jogja</p>
+                  </TitleWrapper>
+                </AvatarWrapper>
+              </Content>
+            </ContentWrapper>
           </MainCover>
         </MainWrapper>
       </AppMain>
     </AppWrapper>
   );
-}
+};
 
 const AppWrapper = styled.div`
   flex: 1 1 auto;
-    -webkit-backface-visibility: hidden;
-    backface-visibility: hidden;
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-    max-width: 100%;
-    position: relative;
-`
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  max-width: 100%;
+  position: relative;
+`;
 
 const AppMain = styled.main`
   display: flex;
   flex: 1 0 auto;
   max-width: 100%;
-  transition: .2s cubic-bezier(.4,0,.2,1);
+  transition: 0.2s cubic-bezier(0.4, 0, 0.2, 1);
   padding: 0;
-`
+`;
 
 const MainWrapper = styled.div`
   flex: 1 1 auto;
   max-width: 100%;
   position: relative;
-`
+`;
 
 const MainCover = styled.div`
   background-image: url(${BgImg});
@@ -56,6 +72,100 @@ const MainCover = styled.div`
   width: 100%;
   margin-left: auto;
   margin-right: auto;
-`
+`;
+
+const ContentWrapper = styled.div`
+  padding-top: 16px;
+  padding-bottom: 200px;
+  margin: 0 auto;
+  justify-content: center;
+  flex-basis: 100%;
+  flex-grow: 0;
+  display: flex;
+  flex: 1 1 auto;
+  flex-wrap: nowrap;
+  min-width: 0;
+  max-width: 100%;
+`;
+
+const Content = styled.div`
+  max-width: 100%;
+  width: calc(512px + 24px * 2);
+`;
+
+const AvatarWrapper = styled.div`
+  color: #fff;
+  text-align: center;
+  margin-top: 0;
+  margin-bottom: 20px;
+  align-items: center;
+  flex-direction: column;
+  position: relative;
+  display: flex;
+  flex: 1 1 auto;
+  flex-wrap: nowrap;
+  min-width: 0;
+`;
+
+const ProfilePicture = styled.div`
+  border-radius: 50%;
+  height: 48px;
+  width: 48px;
+  line-height: 48px;
+  font-size: 40px;
+  font-weight: 300;
+  @media (max-width: 1263px) {
+    height: 64px;
+    width: 64px;
+    line-height: 64px;
+  }
+`;
+
+const Avatar = styled.div`
+  position: relative;
+  overflow: hidden;
+  flex: 1 0 auto;
+  max-width: 100%;
+  display: flex;
+  z-index: 0;
+  .responsive-sizer {
+    transition: padding-bottom 0.2s cubic-bezier(0.25, 0.8, 0.5, 1);
+    flex: 1 0 0px;
+    padding-bottom: 100%;
+  }
+  .ava {
+    z-index: -1;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url(${Logo});
+    background-position: center center;
+    background-color: rgba(0, 0, 0, 0.1);
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+`;
+
+const TitleWrapper = styled.div`
+  @media (max-width: 1024px) {
+    padding-left: 24px;
+    padding-right: 24px;
+  }
+  .display-3 {
+    margin-top: 12px;
+    text-align: center;
+    font-size: 24px;
+    line-height: 28px;
+    word-break: break-word;
+  }
+  .subtitle {
+    margin-top: 8px;
+    font-size: 16px;
+    line-height: 18px;
+    text-align: center;
+  }
+`;
 
 export default App;
